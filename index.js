5651c55fe40bfad64ab9d7e227088f6da4edbb42
Danny Fung <dannyfung97@gmail.com>
2024-02-14 14:30:56 -0800

    installed path, maked public folder static

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 // Function to generate SVG content for the countdown image
 function generateCountdownSVG() {
     const now = moment();
