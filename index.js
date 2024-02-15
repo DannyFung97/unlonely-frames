49f0e5b62bfb63a8882e498252f1f70548dc88ac
Danny Fung <dannyfung97@gmail.com>
2024-02-15 09:22:59 -0800

    call itself again

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.get('/', (req, res) => {
     <meta property="fc:frame:image" content="${imageUrl}" />
 
     <meta property="fc:frame:button:1" content="refresh timer" />
-    <meta property"fc:frame:post_url" content="${imageUrl}" />
+    <meta property"fc:frame:post_url" content="${req.protocol}://${req.get('host')}/" />
 
     <meta property="fc:frame:button:2" content="watch now" />
     <meta property="fc:frame:button:2:action" content="link" />
